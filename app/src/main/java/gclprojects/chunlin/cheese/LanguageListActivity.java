ed4c7952e29fe97fb5368fae47e6ad0db8e0f489
goh-chunlin <chunlin@live.com.sg>
2016-11-06 16:36:43 +0800

    Removed unnecessary menu related events.

--- app/src/main/java/gclprojects/chunlin/cheese/LanguageListActivity.java
+++ app/src/main/java/gclprojects/chunlin/cheese/LanguageListActivity.java
@@ -42,23 +42,6 @@ public class LanguageListActivity extends ListActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_language_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void populateLanguageList() {
         languageList = new ArrayList<>();
 
--- app/src/main/java/gclprojects/chunlin/cheese/MainActivity.java
+++ app/src/main/java/gclprojects/chunlin/cheese/MainActivity.java
@@ -7,8 +7,6 @@ import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -54,23 +52,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
